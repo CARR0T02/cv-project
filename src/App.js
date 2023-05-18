40feaed09002a8fa4a20933a57fcf5028da05253
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-05-18 10:46:33 +0800

    Add Work Ex to main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import PersonalInfo from './components/PersonalInfo';
+import Education from './components/Education';
+import WorkEx from './components/WorkEx';
 
 function App() {
-  return;
+  return (
+    <div className="main">
+      <PersonalInfo />
+      <Education />
+      <WorkEx />
+    </div>
+  );
 }
 
 export default App;
